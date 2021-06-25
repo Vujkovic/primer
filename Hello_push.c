366491293e1aa9c96269bc751898943bd1f6fb08
Vujkovic <vujko.popovic@gmail.com>
2021-06-25 05:51:45 -0700

    cloniran repozitorijum

---
+++ Hello_push.c
@@ -0,0 +1,10 @@
+--- a/hello_git.c
++++ b/hello_git.c
+@@ -8,7 +8,7 @@
+int main() {
+ // Make changes to string constant parameter of 
+printf function
+- printf("Hello git!\n");
++ printf("Hello push!\n");
+ return 0;
+}
